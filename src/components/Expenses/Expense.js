1c8690c96e0f7aaca509c1c694e47226007c3e41
sadaf0611 <sadaf0611@gmail.com>
2023-07-29 08:26:19 -0700

    Rendering Expenses with performance optimisations

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -7,15 +7,24 @@ const Expense = (props) => {
   const [filteredYear,setFilteredYear]= useState('2020');
 
   const filterChangeHandler = selectedYear =>{
-    setFilteredYear(selectedYear);
+    setFilteredYear(selectedYear); 
   };
   return (
     <div className='expenses'>
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      <ExpenceItem title={props.items[0].title} loc={props.items[0].LocationOfExpenditure} amount={props.items[0].amount} date={props.items[0].date}/> 
-      <ExpenceItem title={props.items[1].title} loc={props.items[1].LocationOfExpenditure} amount={props.items[1].amount} date={props.items[1].date}/>
-      <ExpenceItem title={props.items[2].title} loc={props.items[2].LocationOfExpenditure} amount={props.items[2].amount} date={props.items[2].date}/>
-      <ExpenceItem title={props.items[2].title} loc={props.items[2].LocationOfExpenditure} amount={props.items[3].amount} date={props.items[3].date}/>
+      {props.items.map((expense)=>(
+      <ExpenceItem  
+          key = {expense.id}
+          title={expense.title} 
+          loc={expense.LocEx} 
+          amount={expense.amount} 
+          date={expense.date}  />
+      ))}
+
+      {/* <ExpenceItem title={props.items[0].title} loc={props.items[0].LocEx} amount={props.items[0].amount} date={props.items[0].date}/> 
+      <ExpenceItem title={props.items[1].title} loc={props.items[1].LocEx} amount={props.items[1].amount} date={props.items[1].date}/>
+      <ExpenceItem title={props.items[2].title} loc={props.items[2].LocEx} amount={props.items[2].amount} date={props.items[2].date}/>
+      <ExpenceItem title={props.items[2].title} loc={props.items[2].LocEx} amount={props.items[3].amount} date={props.items[3].date}/> */}
     </div>
   )
 }
