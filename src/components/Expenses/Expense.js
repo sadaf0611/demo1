bf26f4f8f47fe94b80095845c2f3351c674388c6
sadaf0611 <sadaf0611@gmail.com>
2023-07-30 07:10:16 +0530

    Conditional Rendering

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -13,28 +13,28 @@ const Expense = (props) => {
   const filteredExpense=props.items.filter(expense=>{
     return expense.date.getFullYear().toString()===filteredYear;
   })
+
+  let expensesContent =   <p>NO data available</p>;
+  if(filteredExpense.length>0){
+     expensesContent=(filteredExpense.map((expense)=>(
+        
+      <ExpenceItem  
+      key = {expense.id}
+      title={expense.title} 
+      loc={expense.LocEx} 
+      amount={expense.amount} 
+      date={expense.date}  />
+    )
+  ));
+  }
+
   return (
     <div className='expenses'>
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {
-        filteredExpense.length===0 
-        ? <p>NO data available</p>
-        : (filteredExpense.map((expense)=>(
-        
-          <ExpenceItem  
-          key = {expense.id}
-          title={expense.title} 
-          loc={expense.LocEx} 
-          amount={expense.amount} 
-          date={expense.date}  />
-        )
-      ))}
-
-
-      {/* <ExpenceItem title={props.items[0].title} loc={props.items[0].LocEx} amount={props.items[0].amount} date={props.items[0].date}/> 
-      <ExpenceItem title={props.items[1].title} loc={props.items[1].LocEx} amount={props.items[1].amount} date={props.items[1].date}/>
-      <ExpenceItem title={props.items[2].title} loc={props.items[2].LocEx} amount={props.items[2].amount} date={props.items[2].date}/>
-      <ExpenceItem title={props.items[2].title} loc={props.items[2].LocEx} amount={props.items[3].amount} date={props.items[3].date}/> */}
+      <ExpenseFilter 
+      selected={filteredYear} 
+      onChangeFilter={filterChangeHandler}/>
+      
+      {expensesContent}  
     </div>
   )
 }
